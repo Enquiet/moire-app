cff996b42a5126385e0116d7fa735e80453be555
unknown <azko1998@mail.ru>
2021-03-04 20:23:40 +0300

    добавил try catch

--- src/api/api.js
+++ src/api/api.js
@@ -2,10 +2,14 @@ import { API_URL } from './url.js'
 
 export default {
   async fetchApi (url, method, data) {
-    const response = await fetch(`${API_URL}/${url}`, {
-      method
-    })
-    const json = await response.json()
-    return json
+    try {
+      const response = await fetch(`${API_URL}/${url}`, {
+        method
+      })
+      const json = await response.json()
+      return json
+    } catch (e) {
+      console.log(e)
+    }
   }
 }
